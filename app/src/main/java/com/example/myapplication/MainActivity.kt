7ccb5ea303c965b089cbdcc5c9cb8ca3c380a1ac
artyom <ryazancev_0101@mail.ru>
2024-11-11 09:05:04 +0300

    22

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,5 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+    }dsfdsafsdfasdfsadfsdafasdfsadfasdfasdfsadfas
 }
